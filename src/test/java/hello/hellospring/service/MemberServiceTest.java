e42921f7e5a6672fce770bb3bc730683576896d6
TaeWooKim-SCH <zop1234@hanmail.net>
2023-11-05 17:47:55 +0900

    feat: 회원 서비스 테스트 코드

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -1,13 +1,28 @@
 package hello.hellospring.service;
 
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MemberServiceTest {
-
+    MemberService memberService = new MemberService();
     @Test
     void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    void DuplicateMemberException() {
+
     }
 
     @Test
